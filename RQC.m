d5de30b2d5d353527f1bb92cf0013ce137c53ca9
marvinseifert <m.seifert@sussex.ac.uk>
2019-03-12 09:50:27 +0000

    Response quality criteria function: Calculates the quality of responses based on the method used in Baden et al. 2016.

---
+++ RQC.m
@@ -0,0 +1,34 @@
+function Quality_criteria = RQC (Epochs)
+Data = create_trial_responses(Epochs);
+Data_size = size(Data);
+STx = Data_size(1);
+nr_epochs = Data_size(3);
+
+
+Quality_criteria = zeros(STx,1);
+
+
+for ii = 1:STx
+    for ss = 1:nr_epochs
+        
+        Data_temp = squeeze(Data(ii,:,ss,:));
+        Data_var = var(Data_temp,0,2,'omitnan');
+        mean_of_var = nanmean(Data_var);
+        Data_mean = nanmean(Data_temp,2);
+        var_of_mean = var(Data_mean,0,1,'omitnan');
+         
+        
+        
+        Quality_criteria(ii,1) = var_of_mean/mean_of_var;
+    end
+end
+        
+
+
+
+
+
+
+
+
+end
